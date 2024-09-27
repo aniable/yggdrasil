33de2ec2733d634a45003a860555ab82aa3f28b0
Sean O'Connor <so@seano.dev>
2024-09-26 18:37:46 -0600

    Clean up logic for handling and building errors

--- src/main/kotlin/com/aniable/yggdrasil/error/StatusPages.kt
+++ src/main/kotlin/com/aniable/yggdrasil/error/StatusPages.kt
@@ -21,31 +21,35 @@ package com.aniable.yggdrasil.error
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.statuspages.*
-import io.ktor.server.request.*
 import io.ktor.server.response.*
 import kotlinx.datetime.Clock
+
+private fun buildErrorResponse(call: ApplicationCall, status: HttpStatusCode, cause: Throwable? = null): ErrorResponse {
+	return ErrorResponse(
+		statusCode = status.value,
+		statusReason = status.description,
+		message = cause?.message,
+		path = call.parameters["path"],
+		timestamp = Clock.System.now()
+	)
+}
 
 fun Application.configureStatusPages() {
 	install(StatusPages) {
 		exception<Throwable> { call, cause ->
-			val status = if (cause is ResponseStatusException) cause.status else HttpStatusCode.InternalServerError
-			val error = ErrorResponse(
-				statusCode = status.value,
-				statusReason = status.description,
-				message = cause.message,
-				path = call.parameters["path"],
-				timestamp = Clock.System.now()
-			)
+			val status = HttpStatusCode.InternalServerError
+			val error = buildErrorResponse(call, status, cause)
+			call.respond(status, error)
+		}
+
+		exception<ResponseStatusException> { call, cause ->
+			val status = cause.status
+			val error = buildErrorResponse(call, status, cause)
 			call.respond(status, error)
 		}
 
 		status(HttpStatusCode.NotFound) { call, status ->
-			val error = ErrorResponse(
-				statusCode = status.value,
-				statusReason = status.description,
-				path = call.request.path(),
-				timestamp = Clock.System.now()
-			)
+			val error = buildErrorResponse(call, status)
 			call.respond(status, error)
 		}
 	}
