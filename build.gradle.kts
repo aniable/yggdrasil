5062966fb439b3316055bf80a9dd4a427007e05a
Sean O'Connor <so@seano.dev>
2024-09-27 15:39:25 -0600

    Add dependency "ktor-server-request-validation"

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,46 +1,47 @@
 plugins {
-    alias(libs.plugins.kotlin.jvm)
-    alias(libs.plugins.ktor)
-    alias(libs.plugins.kotlin.serialization)
+	alias(libs.plugins.kotlin.jvm)
+	alias(libs.plugins.ktor)
+	alias(libs.plugins.kotlin.serialization)
 }
 
 group = "com.aniable"
 version = "0.0.1"
 
 application {
-    mainClass.set("io.ktor.server.netty.EngineMain")
+	mainClass.set("io.ktor.server.netty.EngineMain")
 
-    val isDevelopment: Boolean = project.ext.has("development")
-    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+	val isDevelopment: Boolean = project.ext.has("development")
+	applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
 }
 
 repositories {
-    mavenCentral()
+	mavenCentral()
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-netty-jvm")
-    implementation("io.ktor:ktor-server-core-jvm")
-    implementation("io.ktor:ktor-server-auth-jvm")
-    implementation("io.ktor:ktor-server-auth-jwt-jvm")
-    implementation("io.ktor:ktor-server-host-common-jvm")
-    implementation("io.ktor:ktor-server-status-pages-jvm")
-    implementation("io.ktor:ktor-server-cors-jvm")
-    implementation("io.ktor:ktor-server-default-headers-jvm")
-    implementation("io.ktor:ktor-server-forwarded-header-jvm")
-    implementation("io.ktor:ktor-server-caching-headers-jvm")
-    implementation("io.ktor:ktor-server-compression-jvm")
-    implementation("io.ktor:ktor-server-call-logging-jvm")
-    implementation("io.ktor:ktor-server-content-negotiation-jvm")
-    implementation("io.ktor:ktor-server-config-yaml")
-    implementation("io.ktor:ktor-client-core-jvm")
-    implementation("io.ktor:ktor-client-apache-jvm")
-    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
-    testImplementation("io.ktor:ktor-server-test-host-jvm")
+	implementation("io.ktor:ktor-server-netty-jvm")
+	implementation("io.ktor:ktor-server-core-jvm")
+	implementation("io.ktor:ktor-server-auth-jvm")
+	implementation("io.ktor:ktor-server-auth-jwt-jvm")
+	implementation("io.ktor:ktor-server-host-common-jvm")
+	implementation("io.ktor:ktor-server-status-pages-jvm")
+	implementation("io.ktor:ktor-server-cors-jvm")
+	implementation("io.ktor:ktor-server-default-headers-jvm")
+	implementation("io.ktor:ktor-server-forwarded-header-jvm")
+	implementation("io.ktor:ktor-server-caching-headers-jvm")
+	implementation("io.ktor:ktor-server-compression-jvm")
+	implementation("io.ktor:ktor-server-call-logging-jvm")
+	implementation("io.ktor:ktor-server-content-negotiation-jvm")
+	implementation("io.ktor:ktor-server-config-yaml")
+	implementation("io.ktor:ktor-server-request-validation")
+	implementation("io.ktor:ktor-client-core-jvm")
+	implementation("io.ktor:ktor-client-apache-jvm")
+	implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
+	testImplementation("io.ktor:ktor-server-test-host-jvm")
 
-    implementation(libs.logback.classic)
-    implementation(libs.bundles.exposed)
-    implementation(libs.h2)
-    implementation(libs.bundles.koin)
-    testImplementation(libs.kotlin.test.junit)
+	implementation(libs.logback.classic)
+	implementation(libs.bundles.exposed)
+	implementation(libs.h2)
+	implementation(libs.bundles.koin)
+	testImplementation(libs.kotlin.test.junit)
 }
