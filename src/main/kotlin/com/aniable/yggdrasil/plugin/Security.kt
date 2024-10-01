b136205c0cb5b41a2b83f4a7fd95a18759970d41
Sean O'Connor <so@seano.dev>
2024-10-01 17:41:14 -0600

    Configure JWT authentication

--- src/main/kotlin/com/aniable/yggdrasil/plugin/Security.kt
+++ src/main/kotlin/com/aniable/yggdrasil/plugin/Security.kt
@@ -18,8 +18,31 @@
 
 package com.aniable.yggdrasil.plugin
 
+import com.aniable.yggdrasil.error.ResponseStatusException
+import com.aniable.yggdrasil.security.JwtService
+import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.*
+import org.koin.ktor.ext.inject
 
 fun Application.configureSecurity() {
+	val jwtService by inject<JwtService>()
 
+	install(Authentication) {
+		jwt {
+			verifier(jwtService.verifier())
+			validate { jwtCredential ->
+				val payload = jwtCredential.payload
+				if (payload.subject.isNullOrBlank()) {
+					null
+				} else {
+					JWTPrincipal(payload)
+				}
+			}
+			challenge { _, _ ->
+				throw ResponseStatusException(HttpStatusCode.Unauthorized, "JWT token is not valid")
+			}
+		}
+	}
 }
